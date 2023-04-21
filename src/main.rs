f4cd716368d0fdbc143a633aacd9b20cf1364498
miyamoto <miyamoto.git@gmail.com>
2023-04-22 08:02:05 +0900

    windowおよびグリッド線の実装

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,71 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy_prototype_debug_lines::*;
+
+pub const WINDOW_WIDTH: f32 = 500.0;
+pub const WINDOW_HEIGHT: f32 = 500.0;
+
+pub const GRID_X_LENGTH: u32 = 8;
+pub const GRID_Y_LENGTH: u32 = 8;
 
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "reversi!".into(),
+                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+
+        .add_system(bevy::window::close_on_esc)
+
+        .add_plugin(DebugLinesPlugin::default())
+        .add_system(draw_grid)
+
+        .add_startup_system(spawn_camera)
+
         .run();
+}
+
+pub fn spawn_camera(mut commands: Commands){
+    commands.spawn(Camera2dBundle::default());
+}
+
+pub fn draw_grid(
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut lines: ResMut<DebugLines>
+) {
+    let window = window_query.get_single().unwrap();
+    let half_win_width = 0.5 * window.width();
+    let half_win_height = 0.5 * window.height();
+    let x_space = window.width() / GRID_X_LENGTH as f32;
+    let y_space = window.height() / GRID_Y_LENGTH as f32;
+
+    let mut i = -1. * half_win_height;
+    while i < half_win_height {
+        lines.line(
+            Vec3::new(-1. * half_win_width, i, 0.0),
+            Vec3::new(half_win_width, i, 0.0),
+            0.0,
+        );
+        i += y_space;
+    }
+
+    i = -1. * half_win_width;
+    while i < half_win_width {
+        lines.line(
+            Vec3::new(i, -1. * half_win_height, 0.0),
+            Vec3::new(i, half_win_height, 0.0),
+            0.0,
+        );
+        i += x_space;
+    }
+
+    lines.line(
+        Vec3::new(0., -1. * half_win_height, 0.0),
+        Vec3::new(0., half_win_height, 0.0),
+        0.0,
+    );
 }
